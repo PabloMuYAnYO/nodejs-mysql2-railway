6041ddc4b6a689e60881dbcc885457ad54bb35d6
pable <pablorivasliebana@gmail.com>
2023-01-26 00:09:13 +0100

    Preparamos archivos modificados

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
 const { createPool } = require('mysql2/promise')
 
 const pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    port: '3306',
-    database: 'usersdb'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    port: process.env.MYSQL_PORT,
+    database: process.env.MYSQL_DATABASE
 })
 
 module.exports = pool
-
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var router = express.Router();
-const pool = require('../db.js')
+const pool = require('../db.js')/* conexión base datos */
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('Hola mundo desde express')
 });
 
 module.exports = router;
 
-router.get('/ping', async function(req, res, next) {
+router.get('/ping', async function(req, res, next) { /* async tiene que tener await */
   const result = await pool.query('SELECT "hello world" as RESULT');
   console.log(result[0])
   res.json(result[0]);
@@ -17,9 +17,9 @@ router.get('/ping', async function(req, res, next) {
 
 module.exports = router;
 
-router.get('/create', async function(req, res, next) {
-  const [raws] = await pool.query('SELECT * FROM users')
-  res.json(raws);
+router.get('/create', async function(req, res, next) { /*  */
+  const [rows] = await pool.query('SELECT * FROM users')
+  res.json(rows);
 });
 
 module.exports = router;
